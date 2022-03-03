6a26c80bdec91cf148e3b06a6fdaf06a9e9b4167
TevLee <ltwy0311@naver.com>
2022-03-03 16:22:31 +0900

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,75 @@
+import java.util.stream.Stream;
+
+public class Student {
+    //.. FB : 객체 클래스 내부의 변수들은 일반적으로 private으로 선언
+    private int studentId;
+    private String name;
+    private Integer[] scores;
+    private int total;
+    private float avg;
+    private int rank;
+    private String addr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public Integer[] getScores() {
+        return scores;
+    }
+
+    public float getAvg() {
+        return avg;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public Student(int studentId, String name, Integer[] scores, String addr) {
+        this.studentId = studentId;
+        this.name = name;
+        this.scores = scores;
+        //.. FB : 총점(total)과 평균(avg)는 클래스 내 다른 함수에서 데이터 생성 후 접근.
+        this.total = scoreToTotal(this.scores);
+        this.avg = totalToAvg(this.total, this.scores.length);
+        this.rank = 1;
+        this.addr = addr;
+    }
+
+    private int scoreToTotal(Integer[] scores) {
+        return Stream.of(scores).mapToInt(num -> num).sum();
+    }
+
+    private float totalToAvg(int total, int subjectCnt) {
+        return total / subjectCnt;
+    }
+}
